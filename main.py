eb0172de37aedba14fb38130ea0111bca25ba755
EpicPuppy613 <61771471+EpicPuppy613@users.noreply.github.com>
2021-10-19 21:20:34 +0000

    Added a .gitignore

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ Version ALPHA 1.0.1
 """
 print("Initializing...")
 from init import G
-import time as t
 import builtin
 from mods import *
 import saving
 G.rsm__("init")
 print("-------------")
-t.sleep(0.2)
 print(G.short) 
 G.rsm__("version")
 print(G.release + G.version)
